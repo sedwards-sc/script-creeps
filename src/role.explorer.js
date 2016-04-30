042e9f079cab44e1f5279dd8338af529dfa3b450
sedwards-sc <stedwards28@gmail.com>
2016-04-30 01:35:13 -0400

    adding state to explorer memory and test move to next room

--- src/role.explorer.js
+++ src/role.explorer.js
@@ -4,6 +4,18 @@
 
 module.exports = {
 	run(creep) {
-	    creep.say('explorer');
+		//creep.say('explorer');
+		// state 0 is head to next room
+		// state 1 harvest
+		
+		if(creep.memory.state == undefined) {
+			creep.memory.state = 0;
+		}
+
+		console.log(creep.pos);
+		
+		if(creep.memory.state == 0)
+			creep.moveTo(new RoomPosition(5, 7, 'E22S1'));
+		}
 	}
 };
