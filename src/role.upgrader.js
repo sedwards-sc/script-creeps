2ecd7c6758141255548d346ade79982378052eb9
sedwards-sc <stedwards28@gmail.com>
2016-05-06 01:31:35 -0400

    change upgrader behaviour to use dropped energy

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -20,11 +20,13 @@ module.exports = {
 	    }
 	    
 	    if(creep.memory.state == 0) {
-	        // harvest
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
+	        // get energy
+            var droppedEnergy = creep.room.findClosestByRange(FIND_DROPPED_ENERGY);
+			if(droppedEnergy.length >= 1) {
+				if(creep.pickup(droppedEnergy[0]) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(droppedEnergy[0]);
+				}
+			}
         } else {
             // upgrade
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
