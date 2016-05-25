05137c66501a8fa12eabb5574cb4c90c5cff954a
sedwards-sc <stedwards28@gmail.com>
2016-05-25 09:40:58 -0400

    change explorer to go to room to the other side and will drop energy off at a link instead of building or upgrading

--- src/role.explorer.js
+++ src/role.explorer.js
@@ -10,8 +10,8 @@ module.exports = {
 		// state 2 is head back to home room
 		// state 3 is upgrade controller
 		
-		var checkPointAway = new RoomPosition(47, 29, 'E7S23');
-		var checkPointHome = new RoomPosition(4, 25, 'E8S23');
+		var checkPointAway = new RoomPosition(31, 29, 'E9S23');
+		var checkPointHome = new RoomPosition(47, 9, 'E8S23');
 		
 		if(creep.memory.state === undefined) {
 			creep.memory.state = 0;
@@ -49,17 +49,34 @@ module.exports = {
         } else if(creep.memory.state === 2) {
 			creep.moveTo(checkPointHome);
 		} else if(creep.memory.state === 3) {
-			// work
-			var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-			if(targets.length) {
-				if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(targets[0]);
+			// transfer to link if there is one that isn't full
+			var closestLink = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+					filter: (structure) => {
+						return (structure.structureType === STRUCTURE_LINK) && (structure.energy < structure.energyCapacity);
+					}
+			});
+			
+			if(closestLink) {
+				if(creep.transfer(closestLink, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+					creep.moveTo(closestLink);
 				}
 			} else {
-				if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(creep.room.controller);
-				}
+				//drop energy
+				creep.drop(RESOURCE_ENERGY);
 			}
 		}
 	}
 };
+
+// work
+//var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+//if(targets.length) {
+//	if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+//		creep.moveTo(targets[0]);
+//	}
+//} else {
+//	if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+//		creep.moveTo(creep.room.controller);
+//	}
+//}
+
