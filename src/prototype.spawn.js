e0ede645a05474be67d5ae4cb076f17581c9f784
sedwards-sc <stedwards28@gmail.com>
2016-05-30 08:34:22 -0400

    testing roomCreeps length in prototype spawn test

--- src/prototype.spawn.js
+++ src/prototype.spawn.js
@@ -17,6 +17,6 @@ StructureSpawn.prototype.spawnHarvester = function(roomCreeps) {
 	
 	var newName = this.createCreep(harvesterBody, undefined, {role: 'harvester', spawnRoom: this.pos.roomName});
 	console.log('-test Spawning new harvester: ' + newName);
-	console.log('--' + myRoomEnergy + ' ' + JSON.stringify(roomCreeps));
+	console.log('--' + myRoomEnergy + ' ' + roomCreeps.length + ' ' + JSON.stringify(roomCreeps));
 };
 
