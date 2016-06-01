e511040f11f6800848d8efad8888542010415a89
sedwards-sc <stedwards28@gmail.com>
2016-06-01 00:11:08 -0400

    add a carrier to secondary room

--- src/object.rosters.js
+++ src/object.rosters.js
@@ -47,7 +47,7 @@ module.exports = (function(){
 				0, //remoteMiners
 				0, //remoteCarriers
 				1, //miners
-				0, //carriers
+				1, //carriers
 				0, //linkers
 				0, //reinforcers
 				0, //reservers
