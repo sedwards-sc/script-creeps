24f3e54b7a1cb31c662e65eaa7f07c04dab72d0c
sedwards-sc <stedwards28@gmail.com>
2016-06-05 00:11:54 -0400

    add game tick to hostile creep notification message

--- src/main.js
+++ src/main.js
@@ -448,7 +448,8 @@ function defendRoom(roomName) {
     
     if(hostiles.length > 0) {
         var username = hostiles[0].owner.username;
-        Game.notify(`User ${username} spotted in room ${roomName}`);
+		var gameTime = Game.time;
+        Game.notify(`User ${username} spotted in room ${roomName} at ${gameTime}`);
         var towers = Game.rooms[roomName].find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
         towers.forEach(tower => tower.attack(hostiles[0]));
     } else {
