59e1d80c9cf923abb8da1bca6314f743962b6ade
sedwards-sc <stedwards28@gmail.com>
2016-06-18 11:19:40 -0400

    claimer for secondary room

--- src/object.rosters.js
+++ src/object.rosters.js
@@ -51,7 +51,7 @@ module.exports = (function(){
 				1, //linkers
 				0, //reinforcers
 				1, //reservers
-				0, //claimers
+				1, //claimers
 				0, //remoteUpgraders
 				0 //remoteBuilders
 		);
