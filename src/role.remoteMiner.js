fa362d26337223a58cd663ce0a625c7076e53e1c
sedwards-sc <stedwards28@gmail.com>
2016-06-21 08:32:59 -0400

    jshint code cleanup

--- src/role.remoteMiner.js
+++ src/role.remoteMiner.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 /*
  * role.remoteMiner
  */
@@ -7,15 +8,15 @@ module.exports = {
 		//creep.say('rMiner');
 		// state 0 is head to next room
 		// state 1 harvest
-		
+
 		if((creep.memory.rRoomName === undefined) || (creep.memory.rX === undefined) || (creep.memory.rY === undefined)) {
 			return;
 		}
-		
+
 		//var checkPointAway = new RoomPosition(48, 31, 'E7S23');
-		
+
 		var checkPointAway = new RoomPosition(creep.memory.rX, creep.memory.rY, creep.memory.rRoomName);
-		
+
 		if(creep.memory.state === undefined) {
 			creep.memory.state = 0;
 		}
@@ -24,16 +25,17 @@ module.exports = {
 		    creep.say('away pt');
 			creep.memory.state = 1;
 		}
-		
-		
+
+
 		if(creep.memory.state === 0) {
 			creep.moveTo(checkPointAway);
 		} else if(creep.memory.state === 1) {
 	        // harvest
+			var mySource;
 			if(creep.memory.remoteMine === undefined) {
-				var mySource = creep.pos.findClosestByPath(FIND_SOURCES);
+				mySource = creep.pos.findClosestByPath(FIND_SOURCES);
 			} else {
-				var mySource = creep.room.find(FIND_SOURCES)[creep.memory.remoteMine];
+				mySource = creep.room.find(FIND_SOURCES)[creep.memory.remoteMine];
 			}
 			if(creep.harvest(mySource) === ERR_NOT_IN_RANGE) {
 				creep.moveTo(mySource);
