650bb9223380b13910bdc1552406a175d57dc6ae
sedwards-sc <stedwards28@gmail.com>
2016-06-21 08:27:31 -0400

    miner jshint code cleanup

--- src/role.miner.js
+++ src/role.miner.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 /*
  * role.miner
  */
@@ -7,25 +8,25 @@ module.exports = {
         //creep.say('miner');
 		// state 0 is harvest
 	    // state 1 is transfer energy
-	    if(creep.memory.state == undefined) {
+	    if(creep.memory.state === undefined) {
 	        creep.memory.state = 0;
 	    }
-	    
+
 	    if(creep.carry.energy == creep.carryCapacity) {
-			if(creep.memory.state == 0) {
+			if(creep.memory.state === 0) {
 				creep.say('I\'m full!');
 			}
 	        creep.memory.state = 1;
 	    }
-	    
-	    if (creep.carry.energy == 0) {
+
+	    if (creep.carry.energy === 0) {
 			if(creep.memory.state == 1) {
 				creep.say('I\'m empty!');
 			}
 	        creep.memory.state = 0;
 	    }
-	    
-	    if(creep.memory.state == 0) {
+
+	    if(creep.memory.state === 0) {
             // harvest
             var closestSource = creep.pos.findClosestByPath(FIND_SOURCES);
             var harvestReturn = creep.harvest(closestSource);
