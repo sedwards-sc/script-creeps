9d8b766442277cb30a379a172d6ae3fd5d578e31
sedwards-sc <stedwards28@gmail.com>
2016-06-22 00:31:26 -0400

    optimizing miner role
    added source caching for harvesting and used more efficient method of
    finding storage. also added dropping if no storage

--- src/role.miner.js
+++ src/role.miner.js
@@ -28,21 +28,38 @@ module.exports = {
 
 	    if(creep.memory.state === 0) {
             // harvest
-            var closestSource = creep.pos.findClosestByPath(FIND_SOURCES);
-            var harvestReturn = creep.harvest(closestSource);
-            if (harvestReturn === ERR_NOT_IN_RANGE) {
-                creep.moveTo(closestSource);
-            } else if(harvestReturn === OK) {
-                if((creep.memory.spawnRoom === 'E9S27') && (creep.pos.y !== 36) && (creep.pos.y !== 35)) {
-                    creep.move(TOP);
-                }
-            }
+			var mySource;
+
+			if(creep.memory.mySourceId === undefined) {
+				mySource = creep.pos.findClosestByRange(FIND_SOURCES);
+				creep.memory.mySourceId = mySource.id;
+			} else {
+				mySource = Game.getObjectById(creep.memory.mySourceId);
+				if(mySource === null) {
+					delete creep.memory.mySourceId;
+				}
+			}
+
+			if(mySource) {
+				var harvestReturn = creep.harvest(mySource);
+	            if (harvestReturn === ERR_NOT_IN_RANGE) {
+	                creep.moveTo(mySource);
+	            } else if(harvestReturn === OK) {
+	                if((creep.memory.spawnRoom === 'E9S27') && (creep.pos.y !== 36) && (creep.pos.y !== 35)) {
+	                    creep.move(TOP);
+	                }
+	            }
+			}
 		} else if(creep.memory.state == 1) {
-  			// transfer to storage
-  			var closestStorage = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_STORAGE}});
-  			if(creep.transfer(closestStorage, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-  				creep.moveTo(closestStorage);
-  			}
+  			// transfer to storage or drop
+  			var roomStorage = creep.room.storage;
+			if(roomStorage) {
+				if(creep.transfer(roomStorage, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+	  				creep.moveTo(roomStorage);
+	  			}
+			} else {
+				creep.drop(RESOURCE_ENERGY);
+			}
 		} else {
   			creep.memory.state = 0;
   		}
