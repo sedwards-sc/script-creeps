2b381a1d77e1fb309f3e4c6139881ed10853f2b8
sedwards-sc <stedwards28@gmail.com>
2016-06-29 07:59:03 -0400

    adding jshint option to main and rescoping variables in spawn prototypes

--- src/prototype.spawn.js
+++ src/prototype.spawn.js
@@ -4,9 +4,9 @@
  */
 
 StructureSpawn.prototype.spawnHarvester = function(roomCreeps) {
-	var harvesterBody = [WORK,CARRY,MOVE,MOVE];
+	let harvesterBody = [WORK,CARRY,MOVE,MOVE];
 
-	var myRoomEnergyCapacity = Game.rooms[this.pos.roomName].energyCapacityAvailable;
+	let myRoomEnergyCapacity = Game.rooms[this.pos.roomName].energyCapacityAvailable;
 
 	if(roomCreeps && roomCreeps.length > 0) {
 		if(myRoomEnergyCapacity >= 1150) {
@@ -18,21 +18,21 @@ StructureSpawn.prototype.spawnHarvester = function(roomCreeps) {
 		}
 	}
 
-	var harvesters0 = _.filter(roomCreeps, (creep) => creep.memory.hMine === 0);
-	var harvesters1 = _.filter(roomCreeps, (creep) => creep.memory.hMine === 1);
+	let harvesters0 = _.filter(roomCreeps, (creep) => creep.memory.hMine === 0);
+	let harvesters1 = _.filter(roomCreeps, (creep) => creep.memory.hMine === 1);
 
 	if(harvesters0.length < 3) {
-		var newName = this.createCreep(harvesterBody, undefined, {role: 'harvester', hMine: 0, spawnRoom: this.pos.roomName});
+		let newName = this.createCreep(harvesterBody, undefined, {role: 'harvester', hMine: 0, spawnRoom: this.pos.roomName});
 		console.log('Spawning new harvester0 (' + this.pos.roomName + '): ' + newName);
 	} else if(harvesters1.length < 3) {
-		var newName = this.createCreep(harvesterBody, undefined, {role: 'harvester', hMine: 1, spawnRoom: this.pos.roomName});
+		let newName = this.createCreep(harvesterBody, undefined, {role: 'harvester', hMine: 1, spawnRoom: this.pos.roomName});
 		console.log('Spawning new harvester1 (' + this.pos.roomName + '): ' + newName);
 	}
 };
 
 StructureSpawn.prototype.spawnMineralHarvester = function() {
-	var harvesterBody = [WORK,CARRY,MOVE,MOVE];
+	let harvesterBody = [WORK,CARRY,MOVE,MOVE];
 
-	var newName = this.createCreep(harvesterBody, undefined, {role: 'mineralHarvester', spawnRoom: this.pos.roomName});
+	let newName = this.createCreep(harvesterBody, undefined, {role: 'mineralHarvester', spawnRoom: this.pos.roomName});
 	console.log('Spawning new mineral harvester (' + this.pos.roomName + '): ' + newName);
 };
