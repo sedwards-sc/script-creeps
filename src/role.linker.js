a9273a6bca8ce9728594017d3e48d03f1896b3df
sedwards-sc <stedwards28@gmail.com>
2016-07-02 21:47:30 -0400

    code clean up for linker role

--- src/role.linker.js
+++ src/role.linker.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 /*
  * role.linker
  */
@@ -7,41 +8,41 @@ module.exports = {
         //creep.say('linker');
 		// state 0 is get energy from link
 	    // state 1 is transfer energy to storage
-	    if(creep.memory.state == undefined) {
+	    if(creep.memory.state === undefined) {
 	        creep.memory.state = 0;
 	    }
-	    
-	    if(creep.carry.energy == creep.carryCapacity) {
-			if(creep.memory.state == 0) {
+
+	    if(creep.carry.energy === creep.carryCapacity) {
+			if(creep.memory.state === 0) {
 				creep.say('I\'m full!');
 			}
 	        creep.memory.state = 1;
 	    }
-	    
-	    if (creep.carry.energy == 0) {
+
+	    if (creep.carry.energy === 0) {
 			if(creep.memory.state == 1) {
 				creep.say('I\'m empty!');
 			}
 	        creep.memory.state = 0;
 	    }
-	    
-	    if(creep.memory.state == 0) {
+
+	    if(creep.memory.state === 0) {
 	        // find link that isn't empty
 			//var closestLink = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
 			//		filter: (structure) => {
 			//			return (structure.structureType === STRUCTURE_LINK) && (structure.energy >= creep.carryCapacity);
 			//		}
 			//});
-			
+
 			var closestLink = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_LINK}});
-			
+
 			// find storage link
 			//var closestLink = Game.getObjectById('573a6ed5d32c966b71bd066b');
-			
+
             if(!creep.pos.isNearTo(closestLink)) {
                 creep.moveTo(closestLink);
             }
-        } else if(creep.memory.state == 1) {
+        } else if(creep.memory.state === 1) {
 			// transfer energy to storage
 			var closestStorage = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_STORAGE}});
 			if(creep.transfer(closestStorage, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
