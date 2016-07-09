eac5fee417f0da69d4af8739b20f690d1127c5a1
sedwards-sc <stedwards28@gmail.com>
2016-07-09 16:22:29 -0400

    activate mineral harvesters for rooms 1 and 4

--- src/object.rosters.js
+++ src/object.rosters.js
@@ -38,7 +38,7 @@ module.exports = (function(){
 				0, //claimers
 				0, //remoteUpgraders
 				0, //remoteBuilders
-				0 //mineralHarvesters
+				1 //mineralHarvesters
 		);
 
 		this.E9S27 = new RoomRoster(
@@ -92,7 +92,7 @@ module.exports = (function(){
 				0, //claimers
 				0, //remoteUpgraders
 				2, //remoteBuilders
-				0 //mineralHarvesters
+				1 //mineralHarvesters
 		);
 
 		this.E7S24 = new RoomRoster(
