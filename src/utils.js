53137b3ccf689d015291f3994716b6bc5c1d736e
sedwards-sc <stedwards28@gmail.com>
2016-07-11 00:33:59 -0400

    adding calculate creep cost function to utils

--- src/utils.js
+++ src/utils.js
@@ -8,9 +8,20 @@ function isNullOrUndefined(theObject) {
     return (theObject === undefined || theObject === null);
 }
 
+function calculateCreepCost(bodyParts) {
+	let cost = 0;
+	bodyParts.forEach((bodyPart) => {
+		const part = typeof bodyPart === 'string' ? bodyPart : bodyPart.type;
+		cost += BODYPART_COST[part];
+	});
+
+	return cost;
+}
+
 function populateUtils(g) {
     g.undefToZero = undefToZero;
     g.isNullOrUndefined = isNullOrUndefined;
+	g.calculateCreepCost = calculateCreepCost;
 }
 
 exports.populateUtils = populateUtils;
