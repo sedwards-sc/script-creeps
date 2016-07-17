30032c2484a5b2e8f76a354335d1f2236be30ce3
sedwards-sc <stedwards28@gmail.com>
2016-07-16 21:43:42 -0400

    increase minimum wall/rampart defense

--- src/main.js
+++ src/main.js
@@ -547,14 +547,14 @@ function defendRoom(roomName) {
     } else {
 		let ramparts = Game.rooms[roomName].find(FIND_STRUCTURES, {
 				filter: (structure) => {
-					return (structure.structureType == STRUCTURE_RAMPART) && structure.hits < 25000;
+					return (structure.structureType == STRUCTURE_RAMPART) && structure.hits < 50000;
 				}
 		});
 		let sortedRamparts = _.sortBy(ramparts, function(rampart) { return rampart.hits; });
 
 		let walls = Game.rooms[roomName].find(FIND_STRUCTURES, {
 				filter: (structure) => {
-					return (structure.structureType == STRUCTURE_WALL) && structure.hits < 25000;
+					return (structure.structureType == STRUCTURE_WALL) && structure.hits < 50000;
 				}
 		});
 		let sortedWalls = _.sortBy(walls, function(wall) { return wall.hits; });
