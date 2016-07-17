e6a6fbd60faf8b4ccf46b28cd3462d4bd7ec761b
sedwards-sc <stedwards28@gmail.com>
2016-07-17 02:08:22 -0400

    silence reserver

--- src/role.reserver.js
+++ src/role.reserver.js
@@ -1,15 +1,16 @@
+/* jshint esversion: 6 */
 /*
  * role.reserver
  */
 
 module.exports = {
 	run(creep) {
-		creep.say('reserver');
-		
+		//creep.say('reserver');
+
 		if(creep.memory.controllerId === undefined) {
 			return;
 		}
-		
+
 		var controllerToReserve = Game.getObjectById(creep.memory.controllerId);
 		if(creep.reserveController(controllerToReserve) === ERR_NOT_IN_RANGE) {
 			creep.moveTo(controllerToReserve);
