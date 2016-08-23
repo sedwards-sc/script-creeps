a218258574d3142c5f2ea36efd57a97f821f8746
sedwards-sc <stedwards28@gmail.com>
2016-08-22 23:40:12 -0400

    created terminal run function to distribute resources according to a distributiion list

--- src/prototype.terminal.js
+++ src/prototype.terminal.js
@@ -9,3 +9,27 @@ StructureTerminal.prototype.getResourceQuota = function(resourceType) {
 	}
 	return 6000;
 };
+
+StructureTerminal.prototype.run = function() {
+	if(typeof this.memory.distributionList === 'undefined') {
+		return;
+	}
+
+	for(let i in this.memory.distributionList) {
+		let curDistObj = this.memory.distributionList[i];
+
+		if(this.store[curDistObj.mineral] < this.getResourceQuota(curDistObj.mineral)) {
+			continue;
+		}
+
+		let destinationTerminal = Game.rooms[curDistObj.room].terminal;
+
+		if(destinationTerminal.store[curDistObj.mineral] < destinationTerminal.getResourceQuota(curDistObj.mineral)) {
+			let resourceDeficit = destinationTerminal.getResourceQuota(curDistObj.mineral) - destinationTerminal.store[curDistObj.mineral];
+			let transferAmount = Math.min(resourceDeficit, 1000);
+			if(this.send(curDistObj.mineral, transferAmount, curDistObj.room, 'empire distribution') === OK) {
+				break;
+			}
+		}
+	}
+};
