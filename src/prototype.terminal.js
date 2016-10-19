1aa9cd57d58df8df7470807b0ea29437abb97c32
sedwards-sc <stedwards28@gmail.com>
2016-10-18 23:28:31 -0400

    terminals will send extra energy to the room with the lowest amount of stored energy

--- src/prototype.terminal.js
+++ src/prototype.terminal.js
@@ -11,35 +11,54 @@ StructureTerminal.prototype.getResourceQuota = function(resourceType) {
 };
 
 StructureTerminal.prototype.run = function() {
-	if(typeof this.room.memory.distributionList === 'undefined') {
-		return;
-	}
+	let successfulTransfer = false;
 
-	for(let i in this.room.memory.distributionList) {
-		let curDistObj = this.room.memory.distributionList[i];
+	if(typeof this.room.memory.distributionList !== 'undefined') {
+		for(let i in this.room.memory.distributionList) {
+			let curDistObj = this.room.memory.distributionList[i];
 
-		if(undefToZero(this.store[curDistObj.mineral]) < this.getResourceQuota(curDistObj.mineral)) {
-			continue;
-		}
-
-		let destinationTerminal = Game.rooms[curDistObj.room].terminal;
-
-		if(undefToZero(destinationTerminal.store[curDistObj.mineral]) < destinationTerminal.getResourceQuota(curDistObj.mineral)) {
-			let resourceDeficit = destinationTerminal.getResourceQuota(curDistObj.mineral) - undefToZero(destinationTerminal.store[curDistObj.mineral]);
-			let transferAmount = Math.min(resourceDeficit, 1000);
-			if(this.send(curDistObj.mineral, transferAmount, curDistObj.room, 'empire distribution') === OK) {
-			    console.log('normal, ' + curDistObj.room);
-				break;
+			if(undefToZero(this.store[curDistObj.mineral]) < this.getResourceQuota(curDistObj.mineral)) {
+				continue;
 			}
-		}
 
-		if(curDistObj.override === true) {
-			// protect against draining a room or overfilling the other room
-			if((this.room.storage.store.energy > 500000) && (undefToZero(destinationTerminal.store[curDistObj.mineral]) < (destinationTerminal.getResourceQuota(curDistObj.mineral) * 2))) {
-				if(this.send(curDistObj.mineral, 1000, curDistObj.room, 'empire distribution (override)') === OK) {
-				    console.log('normal, ' + curDistObj.room);
+			let destinationTerminal = Game.rooms[curDistObj.room].terminal;
+
+			if(undefToZero(destinationTerminal.store[curDistObj.mineral]) < destinationTerminal.getResourceQuota(curDistObj.mineral)) {
+				let resourceDeficit = destinationTerminal.getResourceQuota(curDistObj.mineral) - undefToZero(destinationTerminal.store[curDistObj.mineral]);
+				let transferAmount = Math.min(resourceDeficit, 1000);
+				if(this.send(curDistObj.mineral, transferAmount, curDistObj.room, 'empire distribution') === OK) {
+				    //console.log('normal, ' + curDistObj.room);
+					successfulTransfer = true;
 					break;
 				}
+			}
+
+			if(curDistObj.override === true) {
+				// protect against draining a room or overfilling the other room
+				if((this.room.storage.store.energy > 500000) && (undefToZero(destinationTerminal.store[curDistObj.mineral]) < (destinationTerminal.getResourceQuota(curDistObj.mineral) * 2))) {
+					if(this.send(curDistObj.mineral, 1000, curDistObj.room, 'empire distribution (override)') === OK) {
+					    //console.log('normal, ' + curDistObj.room);
+						successfulTransfer = true;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	if(!successfulTransfer) {
+		let roomNameNeedingEnergy = this.getRoomNeedsEnergyMost();
+		let roomNeedingEnergy;
+
+		if(roomNameNeedingEnergy) {
+			roomNeedingEnergy = Game.rooms[roomNameNeedingEnergy];
+		} else {
+			return;
+		}
+
+		if((this.room.storage.store.energy > 500000) && (undefToZero(roomNeedingEnergy.terminal.store.energy) < (roomNeedingEnergy.terminal.getResourceQuota(RESOURCE_ENERGY) * 2))) {
+			if(this.send(RESOURCE_ENERGY, 5000, roomNameNeedingEnergy, 'empire distribution (room requires energy most)') === OK) {
+				console.log('room ' + this.room.name + ' sending energy to room ' + roomNameNeedingEnergy);
 			}
 		}
 	}
