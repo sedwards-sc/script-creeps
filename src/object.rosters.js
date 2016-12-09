d10db56e46a543a141566bffd01bec9c28854267
sedwards-sc <stedwards28@gmail.com>
2016-12-09 07:46:44 -0500

    reinforcers for other new room defenses

--- src/object.rosters.js
+++ src/object.rosters.js
@@ -231,7 +231,7 @@ module.exports = (function(){
 				0, //miners
 				0, //carriers
 				0, //linkers
-				0, //reinforcers
+				2, //reinforcers
 				1, //reservers
 				0, //claimers
 				0, //remoteUpgraders
