69169fb7555689bf2aba4eda8f8e4efffd99bccf
sedwards-sc <stedwards28@gmail.com>
2017-03-03 06:48:19 -0500

    new constants for moving and tagging room types

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,12 @@
 
 global.TEST_CONSTANT = 28;
 
+global.DESTINATION_REACHED = -1501;
+global.ROOMTYPE_SOURCEKEEPER = -1601;
+global.ROOMTYPE_CORE = -1602;
+global.ROOMTYPE_CONTROLLER = -1603;
+global.ROOMTYPE_ALLEY = -1604;
+
 global.ALLIES = {
     "Adimus": true,
 };
