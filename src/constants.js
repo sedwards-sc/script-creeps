bbf73ae862ab283345ca634aaa8200744ecf447d
sedwards-sc <stedwards28@gmail.com>
2020-07-27 23:48:07 -0400

    putting customizable constants at the top of the file

--- src/constants.js
+++ src/constants.js
@@ -3,25 +3,25 @@
  * constants
  */
 
+global.TOWER_RESERVE_ENERGY = 400;
+global.MINIMUM_RAMPART_HEALTH = 5000;
+global.MINIMUM_WALL_HEALTH = 5000;
+
+global.CACHE_INVALIDATION_CHANCE = 0.01;
+
 global.USERNAME = _.first(_.toArray(Game.structures)).owner.username;
 
 global.ALLIES = {
-    "Adimus": true,
+	"Adimus": true,
 };
 
 global.FLAG_STRING_COLONY = "colony";
-
-global.TOWER_RESERVE_ENERGY = 400;
-global.MINIMUM_RAMPART_HEALTH = 1000;
-global.MINIMUM_WALL_HEALTH = 1000;
 
 global.DESTINATION_REACHED = -1501;
 global.ROOMTYPE_SOURCEKEEPER = -1601;
 global.ROOMTYPE_CORE = -1602;
 global.ROOMTYPE_CONTROLLER = -1603;
 global.ROOMTYPE_ALLEY = -1604;
-
-global.CACHE_INVALIDATION_CHANCE = 0.01;
 
 global.PRIORITY_EMERGENCY = 0;
 global.PRIORITY_HIGH = 1;
