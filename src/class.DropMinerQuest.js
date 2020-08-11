c8d2d6804fac62cb71ceccfc25b9259500e0b5a3
sedwards-sc <stedwards28@gmail.com>
2020-08-11 13:30:42 -0400

    log dismantle result for debugging

--- src/class.DropMinerQuest.js
+++ src/class.DropMinerQuest.js
@@ -118,7 +118,7 @@ class DropMinerQuest extends Quest {
 		if(invaderCores.length > 0) {
 			let coreTarget = _.first(invaderCores);
 			if(creep.pos.isNearTo(coreTarget)) {
-				creep.dismantle(coreTarget);
+				creep.log(creep.dismantle(coreTarget));
 			} else {
 				creep.blindMoveTo(coreTarget);
 			}
