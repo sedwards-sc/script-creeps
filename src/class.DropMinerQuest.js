63f16a5556798955d483ae1a3ad6367bb338b659
sedwards-sc <stedwards28@gmail.com>
2020-08-11 13:37:37 -0400

    Revert "log dismantle result for debugging"
    This reverts commit c8d2d6804fac62cb71ceccfc25b9259500e0b5a3.

--- src/class.DropMinerQuest.js
+++ src/class.DropMinerQuest.js
@@ -118,7 +118,7 @@ class DropMinerQuest extends Quest {
 		if(invaderCores.length > 0) {
 			let coreTarget = _.first(invaderCores);
 			if(creep.pos.isNearTo(coreTarget)) {
-				creep.log(creep.dismantle(coreTarget));
+				creep.dismantle(coreTarget);
 			} else {
 				creep.blindMoveTo(coreTarget);
 			}
